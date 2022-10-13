5de71bcdfba03d583f6e10ee7dce8b928cbe9c34
Jucier_Santos <juciersantosadm@gmail.com>
2022-10-13 20:04:25 -0300

    Segunda condicional

--- aula_13-10-exemplo-5.js
+++ aula_13-10-exemplo-5.js
@@ -1,8 +1,8 @@
 
 const vidas = 1;
-const idade = 20;
+const idade = 17;
 
-if (vidas >= 1) {
+if (vidas >= 1 && idade >= 18) {
     console.log("Pode jorgar");
 } else {
     console.log("Game Over");
