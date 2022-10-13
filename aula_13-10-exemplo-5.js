a1b5d88c378b8b54d91f3f149dc652871452c044
Jucier_Santos <juciersantosadm@gmail.com>
2022-10-13 20:20:45 -0300

    Verificar idade denovo

--- aula_13-10-exemplo-5.js
+++ aula_13-10-exemplo-5.js
@@ -1,9 +1,9 @@
 
 const nome = "Pedro";
-const anoNascimento = 2010;
+const anoNascimento = 2000;
 
 if (2022 - anoNascimento >= 18) {
-    console.log("Pode jorgar");
+    console.log("Seja bem-vindo! " + nome);
 } else {
     console.log(nome + ": Não pode acessar!");
 }
