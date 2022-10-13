65bcf8a8ac790c0b1987fea9a8065df90a27c793
Jucier_Santos <juciersantosadm@gmail.com>
2022-10-13 20:58:32 -0300

    Novo Código

--- aula_13-10-exemplo-4.js
+++ aula_13-10-exemplo-4.js
@@ -1,11 +1,11 @@
 
-const data = new Date("August 3, 2020 06:15:00");
+const data = new Date("August 3, 2020 19:15:00");
 const hora = data.getHours();
 
 if (hora < 12) {
     console.log ("Bom dia!");
-} else {
+} else if (hora >= 12 && hora <18) {
     console.log("Boa tarde!");
+} else if (hora >= 18) {
     console.log("Boa noite!");
-
 }
