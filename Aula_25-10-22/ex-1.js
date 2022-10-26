6948825173c08963a21b9aced43b818f0be72d1d
Jucier_Santos <juciersantosadm@gmail.com>
2022-10-25 21:41:48 -0300

    Filtro de lista

--- Aula_25-10-22/ex-1.js
+++ Aula_25-10-22/ex-1.js
@@ -1,1 +1,11 @@
 
+let lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
+
+let termoDeBusca = "Th";
+
+let listaFiltrada = lista.filter(function(item) {
+    return item.includes(termoDeBusca);
+
+});
+
+console.table(listaFiltrada);
